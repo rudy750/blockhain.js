07a22b0aa8ada44dc5380a1fbeea2776bdc51e62
Rudy Sarmiento <rudy750@gmail.com>
2022-04-23 23:07:48 -0400

    added some test cases

--- dev/test.js
+++ dev/test.js
@@ -3,10 +3,14 @@ const Blockchain = require('./blockchain');
 const bitcoin = new Blockchain();
 
 bitcoin.createNewBlock(2389, 'FDF3DFOf999', 'DKJF94FF0994');
-bitcoin.createNewBlock(111, 'SDKFJ8484K', 'KDKD93K99DK');
-bitcoin.createNewBlock(2899, 'FF993FF9', 'FKF000KK');
+bitcoin.createNewTransaction(100, 'ALEX89FDSDF99SF', 'JENKDKDF99DFK00DF');
+bitcoin.createNewBlock(234234, '04FGFG99S00', 'DKSFJSDF999218SS');
 
-bitcoin.getLastBlock();
+bitcoin.createNewTransaction(50, 'ALEX89FDSDF99SF', 'JENKDKDF99DFK00DF');
+bitcoin.createNewTransaction(300, 'ALEX89FDSDF99SF', 'JENKDKDF99DFK00DF');
+bitcoin.createNewTransaction(2000, 'ALEX89FDSDF99SF', 'JENKDKDF99DFK00DF');
+//mine another block to add transactions to chain
+
+bitcoin.createNewBlock(9234234, 'ASDFASDF89AS8ASDF8', 'ASDF9ASDF82DS');
 
 console.log(bitcoin);
-console.log(bitcoin.getLastBlock());

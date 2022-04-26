01f51f793285835c61ac258ade4f21e6518652fc
Rudy Sarmiento <rudy750@gmail.com>
2022-04-25 22:37:07 -0400

    added a post and parser

--- dev/api.js
+++ dev/api.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/blockcahin', (req, res) => {});
 
-app.post('/transaction', (req, res) => {});
+app.post('/transaction', (req, res) => {
+  console.log(req.body);
+  res.send(`transaction amount is  ${req.body.amount}`);
+});
 
 app.get('/mine', (req, res) => {});
 

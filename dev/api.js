2f95674fca6a3691ea43ae9fccbfe860d6bc8964
Rudy Sarmiento <rudy750@gmail.com>
2022-04-25 22:43:14 -0400

    fixed typo and return blockchain

--- dev/api.js
+++ dev/api.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const Blockchain = require('./blockchain');
+
+const bitcoin = new Blockchain();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/blockcahin', (req, res) => {});
+app.get('/blockchain', (req, res) => {
+  res.send(bitcoin);
+});
 
 app.post('/transaction', (req, res) => {
   console.log(req.body);

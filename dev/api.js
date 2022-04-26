3d6f7e032cab79b19a34a550f53f1ef0c0c094a7
Rudy Sarmiento <rudy750@gmail.com>
2022-04-25 22:21:51 -0400

    added api file

---
+++ dev/api.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/blockcahin', (req, res) => {});
+
+app.post('/transaction', (req, res) => {});
+
+app.get('/mine', (req, res) => {});
+
+app.listen(3000, () => console.log('listening on port 3000...'));
